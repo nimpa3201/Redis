2d7267f9f74e0e4ae343d7d9b69d7c3e954ee3a2
gwongilnam <nimpa3201@naver.com>
2025-03-07 01:34:53 +0900

    feat: RedisTemplate 사용

---
+++ src/main/java/com/example/redis/ItemDto.java
@@ -0,0 +1,16 @@
+package com.example.redis;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemDto implements Serializable {
+    private String name;
+    private String description;
+    private Integer price;
+}
