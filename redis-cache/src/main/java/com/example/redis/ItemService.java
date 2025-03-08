8e9d71f3ab70cc4b7c2172e2459d2eb80fafc31e
gwongilnam <nimpa3201@naver.com>
2025-03-08 22:59:55 +0900

    feat: 캐싱 전략 사용하기

---
+++ redis-cache/src/main/java/com/example/redis/ItemService.java
@@ -0,0 +1,82 @@
+package com.example.redis;
+
+import com.example.redis.domain.Item;
+import com.example.redis.domain.ItemDto;
+import com.example.redis.repo.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class ItemService {
+    private final ItemRepository itemRepository;
+
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    //Write-Through
+
+    @CachePut(cacheNames = "itemCache", key = "#result.id")
+    public ItemDto create(ItemDto dto) {
+        return ItemDto.fromEntity(itemRepository.save(Item.builder()
+            .name(dto.getName())
+            .description(dto.getDescription())
+            .price(dto.getPrice())
+            .build()));
+    }
+
+    //Cache-Aside
+    @Cacheable(cacheNames = "itemAllCache", key = "getMethodName()")
+    public List<ItemDto> readAll() {
+        return itemRepository.findAll()
+            .stream()
+            .map(ItemDto::fromEntity)
+            .toList();
+    }
+
+    //이 메서드의 결과는 캐싱이 가능하다
+    // cacheNames : 이 메서드로 인해서 만들어질 캐시를 지칭하는 이름
+    // key : 캐시 데이터를 구분하기 위해 활용하는 값
+    @Cacheable(cacheNames = "itemCache", key = "args[0]")
+    public ItemDto readOne(Long id) {
+        log.info("Read One :{}", id);
+        return itemRepository.findById(id)
+            .map(ItemDto::fromEntity)
+            .orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @CachePut(cacheNames = "itemCache", key = "args[0]")
+    @CacheEvict(cacheNames = "itemAllCache", allEntries = true)
+    public ItemDto update(Long id, ItemDto dto) {
+        Item item = itemRepository.findById(id)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        item.setName(dto.getName());
+        item.setDescription(dto.getDescription());
+        item.setPrice(dto.getPrice());
+        return ItemDto.fromEntity(itemRepository.save(item));
+    }
+
+
+    @CacheEvict(cacheNames = "itemAllCache", key = "args[0]")
+    public void delete(Long id) {
+        itemRepository.deleteById(id);
+    }
+
+    @Cacheable(cacheNames = "itemSearchCache", key = "{args[0],args[1].pageNumber,args[1].pageSize}")
+    public Page<ItemDto> searchByName(String query, Pageable pageable) {
+        return itemRepository.findAllByNameContains(query, pageable)
+            .map(ItemDto::fromEntity);
+    }
+
+}
---
+++ redis-cache/src/main/java/com/example/redis/config/CacheConfig.java
@@ -0,0 +1,44 @@
+package com.example.redis.config;
+
+
+import org.springframework.cache.Cache;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.CacheKeyPrefix;
+import org.springframework.data.redis.cache.RedisCache;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory){
+        // 설정 구성을 먼저 진행
+        // Redis를 이용해서 Spring Cache를 이용할 때
+        // Redis 관련 설정 모아두는 클래스
+        RedisCacheConfiguration configuration = RedisCacheConfiguration
+            .defaultCacheConfig()
+            // null을 캐싱사는지
+            .disableCachingNullValues()
+            // 기본 캐시 유지 시간 (Time TO Live)
+            .entryTtl(Duration.ofSeconds(10))
+            // 캐시를 구분하는 접두사 설정
+            .computePrefixWith(CacheKeyPrefix.simple())
+            //캐시에 저장할 값을 어떻게 직렬화 / 역직렬화 할것인지
+            .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.java()));
+        return RedisCacheManager
+            .builder(redisConnectionFactory)
+            .cacheDefaults(configuration)
+            .build();
+    }
+
+
+}
